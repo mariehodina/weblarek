04e5e043bdac28d3bcbab1d59635d44bdb997713
Masha Khodina <marie.hodina@yandex.by>
2026-03-24 18:31:51 +0300

    шаг 2 сделан

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,31 @@ export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
+
+export interface IProduct {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
+}
+
+export type TPayment = 'card' | 'cash';
+
+export interface IBuyer {
+    payment: TPayment;
+    email: string;
+    phone: string;
+    address: string;
+}
+
+export interface IOrder extends IBuyer {
+    items: string[];  
+    total: number;    
+}
+
+export interface IOrderResult {
+    id: string;
+    total: number;
+}
